779a4575b7f9c5b6034c331e01e6738ab246c57c
GuilhermeFio <guilhermefioreze909@gmail.com>
2025-05-17 21:37:25 -0300

    workspace organizado

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import 'dotenv/config.js';
+import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import adicionarRotas from './rotas.js';
+
+const servidor = express();
+
+servidor.use(bodyParser.json({ limit: '200mb' }));
+servidor.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+
+servidor.use(cors());
+servidor.use(express.json())
+
+
+
+servidor.listen(process.env.PORTA, () => console.log(`A APi subiu na porta: ${process.env.PORTA}`)) 
---
+++ backend/src/rotas.js
@@ -0,0 +1,5 @@
+/*importar todas as paginas controller aqui*/
+
+export default function adicionarRotas(servidor){
+    servidor.use(/*colocar aqui cada controler importado*/)
+}
